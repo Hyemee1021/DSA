5241e13029d81115ab62ccf57a36f4f88fecdf3b
Hye Mee Park <hyemee1021@gmail.com>
2024-07-05 15:09:53 +1000

    spread operator, splice, rotating array

--- rotateArray.js
+++ rotateArray.js
@@ -4,16 +4,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  //array is pushed to the back by k steps
-  const newArray = [];
-  for (let i = 1; i <= k; i++) {
-    //pop from the back
-    newArray.unshift(nums.pop());
-  }
-  for (let ele of nums) {
-    newArray.push(ele);
-  }
-  return newArray;
+  k = k % nums.length;
+  //in case if k is bigger than nums.length
+
+  //splice elements I need to put front
+  nums.unshift(...nums.splice(nums.length - k));
+  //directly changes the content of original array
+  console.log(nums);
 };
 
 console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
