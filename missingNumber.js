6285dc8780e0e4e83dd0510239ab0bf7c447f687
Hye Mee Park <hyemee1021@gmail.com>
2024-07-31 23:56:14 +1000

    getting total sum of array , using reduce()

--- missingNumber.js
+++ missingNumber.js
@@ -5,8 +5,12 @@
 var missingNumber = function (nums) {
   //range will be [0, number]
   let number = nums.length;
+  //[3,0,1]
+  let total = (number * (number + 1)) / 2;
 
-  const allNumbers = new Array(0, number);
-  //can I make an array of integer from 0 to number?
-  //ex>
+  let sumArr = nums.reduce((sum, num) => sum + num, 0);
+
+  return total - sumArr;
 };
+
+console.log(missingNumber([3, 0, 1]));
