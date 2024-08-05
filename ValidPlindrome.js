5977d7e26fd4f27a52b4815ab8bab79169ff5055
Hye Mee Park <hyemee1021@gmail.com>
2024-08-05 15:20:02 +1000

    String problem - solved with two pointers

--- ValidPlindrome.js
+++ ValidPlindrome.js
@@ -2,37 +2,25 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-  //convert uppcase to lowercase
-  //only alphanumeric
-  //read the same forward and backward
+var isPalindrome = function (s) {
+  //converting uppcase -> lowercase
+  //removing all on-alphanumeric
 
-  //creating two pointers- indicating indices
-  let left = 0
-  let right= s.length-1
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  console.log(s);
 
-  //meeting in the middle but do pass each other
-  while( left < right){
-    
-    while( left < right && !isAlphaNumeric(s[left])){
-      left++
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left <= right) {
+    if (s[left] !== s[right]) {
+      return false;
     }
-
-    while( right > left && !isAlphaNumeric(s[right])){
-      right--
-    }
-
-    if( s[left].toLowerCase() !== s[right].toLowerCase()){
-      return false
-    }
-
-    left++
-    right--
+    left++;
+    right--;
   }
-    return true
+  //return true/false
+  return true;
 };
 
-function isAlphaNumeric(char){
-  return /[a-zA-Z0-9]/.test(char)
-
-}
+console.log(isPalindrome("A man, *a plan,! a canal: Panama"));
