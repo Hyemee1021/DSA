ed5bfd3d5e652e4f17151407276dffed46cb9a0a
Hye Mee Park <hyemee1021@gmail.com>
2024-10-22 23:36:59 +1100

    check if an object has certain property and has truthy value

--- practice.js
+++ practice.js
@@ -1,14 +1,8 @@
-function myFunction(arr) {
-  //  array of object
-  // sort in array
-  // const newArr = [3,2,1]-> newArr.sort((a,b) => a- b)
-  // can I do this?
-  return arr.sort((obj1, obj2) => obj1.b - obj2.b);
+function myFunction(a, b) {
+  if (b in a && a[b]) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-console.log(
-  myFunction([
-    { a: 1, b: 2 },
-    { a: 5, b: 4 },
-  ])
-);
+console.log(myFunction({ x: "a", b: "b", z: undefined }, "z"));
